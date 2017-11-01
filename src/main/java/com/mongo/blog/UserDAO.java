e147fee4e47f2abdea37eaac2dd892be4c8ac373
Alexander Hamza <Impovich2@gmail.com>
2017-11-01 16:05:11 +0100

    initial commit of the blog based on mongo db

---
+++ src/main/java/com/mongo/blog/UserDAO.java
@@ -0,0 +1,83 @@
+package com.mongo.blog;
+
+import com.mongodb.ErrorCategory;
+import com.mongodb.MongoWriteException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Random;
+
+public class UserDAO {
+    private final MongoCollection<Document> usersCollection;
+    private Random random = new SecureRandom();
+
+    public UserDAO(final MongoDatabase blogDatabase) {
+        usersCollection = blogDatabase.getCollection("users");
+    }
+
+    // validates that username is unique and insert into db
+    public boolean addUser(String username, String password, String email) {
+
+        String passwordHash = makePasswordHash(password, Integer.toString(random.nextInt()));
+
+        final Document newUser = new Document().append("_id", username).append("password", passwordHash);
+
+        if (email != null && !email.equals("")) {
+            newUser.append("email", email);
+        }
+
+        try {
+            usersCollection.insertOne(newUser);
+            return true;
+        } catch (MongoWriteException e) {
+            if (e.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
+                System.out.println("Username already in use: " + username);
+                return false;
+            }
+            throw e;
+        }
+    }
+
+    public Document validateLogin(String username, String password) {
+        Document user = usersCollection.find(Filters.eq("_id", username)).first();
+
+        if (user == null) {
+            System.out.println("User not in database");
+            return null;
+        }
+
+        String hashedAndSalted = user.get("password").toString();
+
+        String salt = hashedAndSalted.split(",")[1];
+
+        if (!hashedAndSalted.equals(makePasswordHash(password, salt))) {
+            System.out.println("Submitted password is not a match");
+            return null;
+        }
+
+        return user;
+    }
+
+
+    private String makePasswordHash(String password, String salt) {
+        try {
+            String saltedAndHashed = password + "," + salt;
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.update(saltedAndHashed.getBytes());
+
+            byte hashedBytes[] = (new String(digest.digest(), "UTF-8")).getBytes();
+            return Base64.getEncoder().encodeToString(hashedBytes) + "," + salt;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 is not available", e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("UTF-8 unavailable?  Not a chance", e);
+        }
+    }
+}
